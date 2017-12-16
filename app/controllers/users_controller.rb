888f7630b7e5fe8785f5930776efe933b9e56cd5
Patrick Sanfiel <patricksanfiel@gmail.com>
2017-12-16 17:22:47 -0500

    fixing user authorization and spiking out profile page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :authorize, except:[:new, :create]
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/profile'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def show
+    @user = User.find_by(params[:username])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  resources :users
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'welcome/index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/user' => 'users#show'
+
+
+  root 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

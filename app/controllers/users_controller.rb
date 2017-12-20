2e6804d3294f891e1eddc87458ee14ab9e523fa4
Nestor Guthrie <nestor33125@hotmail.com>
2017-12-20 13:49:52 -0500

    Points Column Add to User Table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
-  before_action :authorize, except:[:new, :create]
-  def new
-  end
+  before_action :authorize, except: %i[new create]
+  def new; end
 
   def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/profile'
+      redirect_to '/user'
     else
       redirect_to '/signup'
     end
@@ -18,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216225613) do
+ActiveRecord::Schema.define(version: 20171220183523) do
 
   create_table "spots", force: :cascade do |t|
     t.float "latitude"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171216225613) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "points"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
